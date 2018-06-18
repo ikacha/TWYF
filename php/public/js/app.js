48614f971bd252f1d522657fc5457ae52a18cf50
VoxSiren <nayshe.nikki@gmail.com>
2018-06-18 11:38:28 +0200

    first

---
+++ php/public/js/app.js
@@ -0,0 +1,86 @@
+
+$(document).ready(function(){
+    $("#sign-btn").click(function(e){
+        e.preventDefault();
+        $("#signuppage").show();
+        $("#menupage").hide();
+        $("#connexionpage").hide();
+    });
+
+    $("#cnx-btn").click(function(e){
+        e.preventDefault();
+        $("#menupage").show();
+        $("#connexionpage").hide();
+        $("#signuppage").hide();
+
+});
+
+$(document).ready(function(){
+    $("#cnx-btn").click(function(e){
+        e.preventDefault();
+        $("#menupage").show();
+        $("#connexionpage").hide();
+        $("#signuppage").hide();
+    });
+});
+
+/*
+
+$.ajax({
+        url: "http://127.0.0.1:8000/home",s
+    }).done(
+        sign.onclick(
+            function showSignup(){
+            $("#signuppage").show();
+            $("#connexionpage").hide();
+}));
+
+
+function showMenu(){
+    $.ajax({
+        url: "http://127.0.0.1:8000/home",
+    }).done(function(){
+            $("#connexionpage").fadeOut(1000, function(){
+            $("#signuppage").fadeIn(1000);
+            $("#signup-btn").show();
+       });
+    })
+}
+/*
+
+$.ajax({
+    function notify() {
+    alert( "clicked" );
+    }
+    $( "signup-btn" ).on( "click", notify );
+});
+
+
+
+function showSignup(){
+
+    $("#signuppage").show();
+    $("#connexionpage").hide();
+}
+
+
+$("#signup-btn").click(function(){
+  showSignup();
+});
+
+
+
+ 
+function click() {
+  console.log( "cliqué" );
+}
+ 
+$( "#signup-btn" ).on( "click", click );
+
+}
+
+function showSignup(){
+
+    $("#signuppage").show();
+    $("#connexionpage").hide();*/
+
--- php/src/Controller/HomeController.php
+++ php/src/Controller/HomeController.php
@@ -3,18 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-    class HomeController extends Controller
+class HomeController extends Controller
 {
     /**
      * @Route("/home", name="home")
      */
     public function index()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HomeController.php',
+        return $this->render('home/index.html.twig', [
         ]);
     }
 }
+
+
